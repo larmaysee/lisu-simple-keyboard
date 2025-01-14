2bc7da09c6ec233e3d9e3b56830a66cadc469239
Lar May See <larmaysee.git@gmail.com>
2025-01-14 16:12:31 +0630

    Initial Commit

---
+++ Lisu Keyboard/Lisu_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lisu_KeyboardApp.swift
+//  Lisu Keyboard
+//
+//  Created by Lar May See on 14/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lisu_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lisu KeyboardTests/Lisu_KeyboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lisu_KeyboardTests.swift
+//  Lisu KeyboardTests
+//
+//  Created by Lar May See on 14/01/2025.
+//
+
+import Testing
+@testable import Lisu_Keyboard
+
+struct Lisu_KeyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
